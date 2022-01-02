0629e8439af2aa12ba2a2a79d9e1215a3d329d31
wodyd202 <wodyd202@daum.net>
2022-01-02 17:03:22 +0900

    [환율 API] 환율 API 구현 완료

---
+++ src/main/java/com/exchangeratecalculator/service/application/ExchangeRateRequester.java
@@ -0,0 +1,15 @@
+package com.exchangeratecalculator.service.application;
+
+import com.exchangeratecalculator.config.exchangerateAPI.ExchangeRateRequestProperties;
+import com.exchangeratecalculator.service.domain.ExchangeRate;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "exchangeRateRequester", url = "${exchange-rate.uri}")
+public interface ExchangeRateRequester {
+    @GetMapping("live?currencies={currencies}")
+    ExchangeRate getExchangeRate(@SpringQueryMap ExchangeRateRequestProperties properties,
+                                 @PathVariable String currencies);
+}
--- src/test/java/com/exchangeratecalculator/infrastructure/ExchangeRateRequester_Test.java
+++ src/test/java/com/exchangeratecalculator/infrastructure/ExchangeRateRequester_Test.java
@@ -1,6 +1,9 @@
 package com.exchangeratecalculator.infrastructure;
 
 import com.exchangeratecalculator.config.exchangerateAPI.ExchangeRateRequestProperties;
+import com.exchangeratecalculator.service.domain.ExchangeRate;
+import com.exchangeratecalculator.service.application.ExchangeRateRequestDTO;
+import com.exchangeratecalculator.service.application.ExchangeRateRequester;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +11,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class ExchangeRateRequester_Test {
-    @Autowired ExchangeRateRequester exchangeRateRequester;
+    @Autowired
+    ExchangeRateRequester exchangeRateRequester;
     @Autowired ExchangeRateRequestProperties exchangeRateRequesterDTO;
 
     @Test
     @DisplayName("환율정보 요청")
     void getExchangeRate(){
-        ExchangeRateResponse o = exchangeRateRequester.getExchangeRate(exchangeRateRequesterDTO);
+        ExchangeRateRequestDTO exchangeRateRequestDTO = new ExchangeRateRequestDTO("KRW,JPY,PHP");
+        ExchangeRate o = exchangeRateRequester.getExchangeRate(exchangeRateRequesterDTO, exchangeRateRequestDTO.getCurrencies());
         System.out.println(o);
     }
 }
